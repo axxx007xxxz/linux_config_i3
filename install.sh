f98b0435c8f01e0f00a82ba630698a64add1457e
axxx007xxxz <mattiabrsrn7@gmail.com>
2020-02-29 20:08:16 +0100

    Add an installation script
    * Very simple script that copies files into their respective destinations
    * Abort if run as root as it copies file into home.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $UID == "0" ]]; then
+	echo "ERROR: You must run this script as user!"
+	exit 1
+fi
+
+# Copy configuration files
+cp i3/config ~/.config/i3/config
+
+# Reload current configurations
+i3-msg reload
+
+echo "Done."
