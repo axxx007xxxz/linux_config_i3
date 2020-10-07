b763fd198230811bae3187a87604e4cb7b408a9b
axxx007xxxz <mattiabrsrn7@gmail.com>
2020-10-07 16:00:37 +0200

    install.sh: Make directories if needed

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-
 if [[ $UID == "0" ]]; then
 	echo "ERROR: You must run this script as user!"
 	exit 1
+fi
+
+
+# Make the directories if needed
+if [[ ! -d "~/.config/i3" ]]; then
+	mkdir ~/.config/i3
+fi
+if [[ ! -d "~/.config/i3status" ]]; then
+	mkdir ~/.config/i3status
 fi
 
 # Copy configuration files
